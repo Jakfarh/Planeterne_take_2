ec7378d2045d1bf90b4d8eff034a92d69f127a08
jakob <jakob@DESKTOP-BEG0Q6G>
2021-09-05 21:04:08 +0200

    Add project files.

---
+++ Planeterne_take_2/PlanetDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeterne_take_2
+{
+    public class PlanetDatabase
+    {
+        private string planetname;
+
+        Planetcreator Mercury = new Planetcreator("Mercury", 0.330, 4879, 5427, 3.7, 1407.6, 4222.6, 57.9, 88.0, 47.4, 167, 0, false);
+        Planetcreator Venus = new Planetcreator("Venus", 4.87, 12104, 5243, 8.9, -5832.5, 2802.0, 108.2, 224.7, 35.0, 464, 0, false);
+        Planetcreator Earth = new Planetcreator("Earth", 5.97, 12756, 5514, 9.8, 23.9, 24.7, 149.6, 365.2, 29.8, 15, 1, false);
+        Planetcreator Mars = new Planetcreator("Mars", 0.642, 6792, 3933, 3.7, 24.6, 24.7, 227.9, 687.0, 24.1, -65, 2, false);
+        Planetcreator Jupiter = new Planetcreator("Jupiter", 1898, 142984, 1326, 23.1, 9.9, 9.9, 778.6, 4331, 13.1, -110, 67, true);
+        Planetcreator Saturn = new Planetcreator("Saturn", 568, 120536, 687, 9.0, 10.7, 10.7, 1433.5, 10757, 9.7, -140, 62, true);
+        Planetcreator Uranus = new Planetcreator("Uranus", 86.8, 51118, 1271, 8.7, -17.2, 17.2, 2872.5, 30589, 6.8, -195, 27, true);
+        Planetcreator Neptune = new Planetcreator("Neptune", 102, 49528, 1638, 11.0, 16.1, 16.1, 4495.1, 59800, 5.4, -200, 14, true);
+        Planetcreator Pluto = new Planetcreator("Pluto", 0.0146, 2370, 2095, 0.7, -153.3, 153.3, 5906.4, 90560, 4.7, -225, 5, false);
+    }
+    public static class Planetlisting
+    {
+        public static List<PlanetDatabase> Planetlist
+            private string name
+            public string Name (PlanetDatabase.)
+        {
+            get { return PlanetDatabase.}
+
+    }
+
+
+
+
+
+    }
---
+++ Planeterne_take_2/Planetcreator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeterne_take_2
+{
+    class Planetcreator
+    {
+        private string name;
+        private double mass;
+        private double diameter;
+        private double density;
+        private double gravity;
+        private double rotation_period;
+        private double length_of_day;
+        private double distance_from_sun;
+        private double orbital_period;
+        private double orbital_velocity;
+        private int mean_temperature;
+        private int number_of_moons;
+        private bool ring_system;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+        public double Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; }
+        }
+        public double Density
+        {
+            get { return density; }
+            set { density = value; }
+        }
+        public double Gravity
+        {
+            get { return gravity; }
+            set { gravity = value; }
+        }
+        public double Rotation_Period
+        {
+            get { return rotation_period; }
+            set { rotation_period = value; }
+        }
+        public double Length_of_Day
+        {
+            get { return length_of_day; }
+            set { length_of_day = value; }
+        }
+        public double Distance_from_Sun
+        {
+            get { return distance_from_sun; }
+            set { distance_from_sun = value; }
+        }
+        public double Orbital_Period
+        {
+            get { return orbital_period; }
+            set { orbital_period = value; }
+        }
+        public double Orbital_Velocity
+        {
+            get { return orbital_velocity; }
+            set { orbital_velocity = value; }
+        }
+        public int Mean_Temperature
+        {
+            get { return mean_temperature; }
+            set { mean_temperature = value; }
+        }
+        public int Number_of_Moons
+        {
+            get { return number_of_moons; }
+            set { number_of_moons = value; }
+        }
+        public bool Ring_System
+        {
+            get { return ring_system; }
+            set { ring_system = value; }
+        }
+        public Planetcreator(string _Name, double _Mass, double _Diameter, double _Density, double _Gravity, double _Rotation_Period, double _Length_of_Day, double _Distance_from_Sun, double _Orbital_Period, double _Orbital_Valocity, int _Mean_Temperature, int _Number_of_Moons, bool _Ring_System)
+        {
+            Name = _Name;
+            Mass = _Mass;
+            Diameter = _Diameter;
+            Density = _Density;
+            Gravity = _Gravity;
+            Rotation_Period = _Rotation_Period;
+            Length_of_Day = _Length_of_Day;
+            Distance_from_Sun = _Distance_from_Sun;
+            Orbital_Period = _Orbital_Period;
+            Orbital_Velocity = _Orbital_Valocity;
+            Mean_Temperature = _Mean_Temperature;
+            Number_of_Moons = _Number_of_Moons;
+            Ring_System = _Ring_System;
+        }
+    }
+}
